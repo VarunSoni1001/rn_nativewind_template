34d30455cd325a61dd246a7ac3ebd00f78e03d41
Varun Soni <varun1001soni@gmail.com>
2024-08-10 14:23:42 +0530

    initial

---
+++ app/index.tsx
@@ -0,0 +1,24 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import { useRouter } from "expo-router";
+
+export default function Home() {
+  const router = useRouter();
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack">Medicause!</Text>
+      <View className="flex-row items-center justify-between w-full px-6">
+      <TouchableOpacity onPress={() => router.push("/profile")}>
+        <Text className="px-3 py-1 bg-black rounded-full text-white">
+          Go to Profile
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => router.push("/settings")}>
+        <Text className="px-3 py-1 bg-black rounded-full text-white">
+          Go to Settings
+        </Text>
+      </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
---
+++ app/profile.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const profile = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-slate-200">
+      <Text className="font-psemibold">profile</Text>
+    </View>
+  );
+};
+
+export default profile;
